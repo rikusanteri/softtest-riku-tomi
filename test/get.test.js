298d5b5c69fdbb9ed072d4956d80aa93f01e11fd
Riku Rainio <riku.rainio@tuni.fi>
2021-12-04 21:28:28 +0200

    add base tests for get.js

--- test/get.test.js
+++ test/get.test.js
@@ -1,2 +1,22 @@
 import chai from 'chai';
 import get from '../src/get.js';
+import assert from 'assert';
+const expect = chai.expect;
+
+describe('the get function', () => {
+    it('should return the path of object if the resolved value is defined', () => {
+        const object = { 'a': [{ 'b': { 'c': 3 } }] }
+        const result = get(object, 'a[0].b.c');
+        expect(result, "to equal").to.equal(3);
+    });
+    it('should return defaultValue if the resolved value is undefined', () => {
+        const object = { 'a': [{ 'b': { 'c': 3 } }] }
+        const result = get(object, 'a.b.c', 'default');
+        expect(result, "to equal").to.equal('default');
+    });
+    it('own test, should return undefined when given no parameters', () => {
+        const object = {}
+        const result = get();
+        expect(result, "to equal").to.be.undefined;
+    });
+});
