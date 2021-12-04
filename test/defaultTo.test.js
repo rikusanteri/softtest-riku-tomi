1abdb00299d3f22c5003e0d626a4d1f688d7c701
Riku Rainio <riku.rainio@tuni.fi>
2021-12-04 20:34:35 +0200

    add basic tests for eq.js

--- test/defaultTo.test.js
+++ test/defaultTo.test.js
@@ -4,15 +4,15 @@ import assert from 'assert'
 const expect = chai.expect;
 
 describe('the defaultTo function', () => {
-    it('should return defaultValue (1 in test) when value is Nan', () => {
+    it('should return defaultValue (1 in test) when value is NaN', () => {
         const result = defaultTo(NaN, 1);
         assert.equal(result, 1);
     });
-    it('should return defaultValue (1 in test) when value is Nan', () => {
+    it('should return defaultValue (1 in test) when value is NaN', () => {
         const result = defaultTo(null, 1);
         assert.equal(result, 1);
     });
-    it('should return defaultValue (1 in test) when value is Nan', () => {
+    it('should return defaultValue (1 in test) when value is NaN', () => {
         const result = defaultTo(undefined, 1);
         assert.equal(result, 1);
     });
--- test/eq.test.js
+++ test/eq.test.js
@@ -1,2 +1,32 @@
 import chai from 'chai';
 import eq from '../src/eq.js';
+import assert from 'assert';
+const expect = chai.expect;
+
+describe('the eq function', () => {
+
+    it('should return true if the values are equivalent', () => {
+        const object = { 'a': 1 };
+        const other = { 'a': 1 };
+        const result = eq(object, object);
+        assert.equal(result, true);
+    });
+    it('should return false if the values are not equivalent', () => {
+        const object = { 'a': 1 };
+        const other = { 'a': 1 };
+        const result = eq(object, other);
+        assert.equal(result, false);
+    });
+    it('should return true if the values are equivalent', () => {
+        const result = eq('a', 'a');
+        assert.equal(result, true);
+    });
+    it('should return false if the values are not equivalent', () => {
+        const result = eq('a', Object('a'));
+        assert.equal(result, false);
+    });
+    it('should return true if the values are equivalent', () => {
+        const result = eq(NaN, NaN);
+        assert.equal(result, true);
+    });
+})
