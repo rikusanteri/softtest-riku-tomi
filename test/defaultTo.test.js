d2b6eb276ce61736ff39b8004921f5fc5414b21c
Riku Rainio <riku.rainio@tuni.fi>
2021-12-04 19:27:03 +0200

    add basic tests for defaultTo.js

--- test/defaultTo.test.js
+++ test/defaultTo.test.js
@@ -1,2 +1,19 @@
 import chai from 'chai';
 import defaultTo from '../src/defaultTo.js';
+import assert from 'assert'
+const expect = chai.expect;
+
+describe('the defaultTo function', () => {
+    it('should return defaultValue (1 in test) when value is Nan', () => {
+        const result = defaultTo(NaN, 1);
+        assert.equal(result, 1);
+    });
+    it('should return defaultValue (1 in test) when value is Nan', () => {
+        const result = defaultTo(null, 1);
+        assert.equal(result, 1);
+    });
+    it('should return defaultValue (1 in test) when value is Nan', () => {
+        const result = defaultTo(undefined, 1);
+        assert.equal(result, 1);
+    });
+})
