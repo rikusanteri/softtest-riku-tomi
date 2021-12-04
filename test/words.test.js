42ae225b66124b8c65e0912a9e1caf519ff9f08e
Riku Rainio <riku.rainio@tuni.fi>
2021-12-04 22:01:11 +0200

    add base tests for words.js

--- test/words.test.js
+++ test/words.test.js
@@ -1,2 +1,17 @@
 import chai from 'chai';
 import words from '../src/words.js';
+import assert from 'assert'
+const expect = chai.expect;
+
+describe('the words function', () => {
+    it('should return an array of words splitted from parameter string', () => {
+        const result = words('fred, barney, & pebbles');
+        expect(result, "to equal").to.eql(['fred', 'barney', 'pebbles'])
+        //assert.equal(result, ['fred', 'barney', 'pebbles']);
+    });
+    it('should return an array of words splitted from parameter string with given pattern', () => {
+        const result = words('fred, barney, & pebbles', /[^, ]+/g);
+        expect(result, "to equal").to.eql(['fred', 'barney', '&', 'pebbles']);
+        //assert.eql(result, ['fred', 'barney', '&', 'pebbles']);
+    });
+});
